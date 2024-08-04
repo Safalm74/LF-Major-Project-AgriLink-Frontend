89f465effbbacece84d32c703c45718d83169199
Safal raj Manandhar <safalm74@gmail.com>
2024-08-04 21:49:58 +0545

    user created toast

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,5 +1,6 @@
 export function debounce(callback: Function, delay: number) {
   let timeoutId: number;
+
   return (...args: any) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
